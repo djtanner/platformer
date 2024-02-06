22a2874b0ba645026f0c1dcdf335cf178e3e76e2
Zhila Shariat <zshariat08@gmail.com>
2024-02-06 00:36:38 -0800

    keyboard moves rect

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,10 +1,17 @@
 package inputs;
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
 
+   private GamePanel gamePanel;
+
+   public KeyboardInputs(GamePanel gamePanel) {
+      this.gamePanel = gamePanel;
+   }
 
    public void keyTyped(KeyEvent e) {
 
@@ -17,19 +24,19 @@ public class KeyboardInputs implements KeyListener {
    public void keyReleased(KeyEvent e) {
       switch (e.getKeyCode()) {
          case KeyEvent.VK_W:
-            System.out.println("W was pressed");
-            break;
-
-         case KeyEvent.VK_A:
-            System.out.println("A was pressed");
+            gamePanel.changeYDelta(-5);
             break;
 
          case KeyEvent.VK_S:
-            System.out.println("S was pressed");
+            gamePanel.changeYDelta(+5);
             break;
 
          case KeyEvent.VK_D:
-            System.out.println("D was pressed");
+            gamePanel.changeXDelta(5);
+            break;
+
+         case KeyEvent.VK_A:
+            gamePanel.changeXDelta(-5);
             break;
       }
    }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,13 +8,27 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
    private MouseInputs mouseInputs;
+   private int xDelta;
+   private int yDelta;
 
    public GamePanel() {
       mouseInputs = new MouseInputs();
-      addKeyListener(new KeyboardInputs());
+      addKeyListener(new KeyboardInputs(this));
       addMouseListener(mouseInputs);
       addMouseMotionListener(mouseInputs);
 
+      xDelta = 0;
+      yDelta = 0;
+   }
+
+   public void changeXDelta(int value) {
+      this.xDelta += value;
+      repaint();
+   }
+
+   public void changeYDelta(int value) {
+      this.yDelta += value;
+      repaint();
    }
 
    public void paintComponent(Graphics g) {
@@ -22,7 +36,7 @@ public class GamePanel extends JPanel {
 
       Graphics2D g2 = (Graphics2D) g;
       g2.setColor(Color.BLUE);
-      g2.fillRect(100, 100, 200, 50);
+      g2.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
 
    }
 
